1b149c0e6cf6af97b1a4959ebbbb64ed2e5571cd
Rayyan <85759101+XfaonAE@users.noreply.github.com>
2021-06-20 20:41:56 -0400

    Changed files to follow formatting rules

--- src/main/index.js
+++ src/main/index.js
@@ -1,83 +1,55 @@
-import { app, BrowserWindow, ipcMain } from 'electron'
-import '../renderer/store'
-
-/**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
- */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+import { app, BrowserWindow, ipcMain } from "electron"
+import "../renderer/store"
+if (process.env.NODE_ENV !== "development") {
+    global.__static = require("path").join(__dirname, "/static").replace(/\\/g, "\\\\");
 }
 
 let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+const winURL = process.env.NODE_ENV === "development"
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`;
 
 function createWindow () {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    height: 700,
-    useContentSize: true,
-    width: 1500,
-    frame: false
-  })
+    mainWindow = new BrowserWindow({
+        height: 700,
+        useContentSize: true,
+        width: 1500,
+        frame: false
+    });
 
-  mainWindow.loadURL(winURL)
+    mainWindow.loadURL(winURL);
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow.on("closed", () => {
+        mainWindow = null
+    });
 
-  ipcMain.on("_ELECTRON_windowClose", (event, data) => {
-    mainWindow.close(); 
-  });
+    ipcMain.on("_ELECTRON_windowClose", (event, data) => {
+        mainWindow.close(); 
+    });
 
-  ipcMain.on("_ELECTRON_windowResize", (event, data) => {
-    if (mainWindow.isMaximized()) {
-      mainWindow.restore();
-      return; 
-    }
-    mainWindow.maximize(); 
-  });
+    ipcMain.on("_ELECTRON_windowResize", (event, data) => {
+        if (mainWindow.isMaximized()) {
+        mainWindow.restore();
+        return; 
+        }
+        mainWindow.maximize(); 
+    });
 
-  ipcMain.on("_ELECTRON_windowMinimize", (event, data) => {
-    mainWindow.minimize(); 
-  });
+    ipcMain.on("_ELECTRON_windowMinimize", (event, data) => {
+        mainWindow.minimize(); 
+    });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow)
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+});
 
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
-
-/*
-import { autoUpdater } from 'electron-updater'
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
+app.on("activate", () => {
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
